c87cc706bcb067f973cfe8d27cce7731670ebeab
baraa102 <elbaraahmed@gmail.com>
2026-03-06 13:37:02 +0400

    Delete V6

--- assets/js/gallery_static.js
+++
@@ -1,6 +0,0 @@
-;window.LUH_GALLERY = [
-  { src: "assets/images/gallery/g1.jpg", alt: "g1" },
-  { src: "assets/images/gallery/g2.jpg", alt: "g2" },
-  { src: "assets/images/gallery/g3.jpg", alt: "g3" },
-  { src: "assets/images/gallery/g4.jpg", alt: "g4" }
-];
